12960d39e936b5036a6bf7ab6a3765458d66106a
XxyarcexX <jjhernandezyarce@gmail.com>
2025-10-20 07:19:03 -0500

    update

--- backend/src/routes/categories.routes.js
+++ backend/src/routes/categories.routes.js
@@ -4,5 +4,7 @@ const categoriesController = require('../controllers/categories.controller');
 
 router.get('/', categoriesController.getCategories);
 router.post('/', categoriesController.createCategory);
+router.put('/:id', categoriesController.updateCategory);
+router.delete('/:id', categoriesController.deleteCategory);
 
 module.exports = router;
