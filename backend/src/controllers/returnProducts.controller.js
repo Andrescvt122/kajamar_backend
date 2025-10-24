146db975d413858fc70b18996e9181f62a0cca82
Andre <andrescvalencia.t@gmail.com>
2025-10-24 18:53:52 -0500

    terminacion de getOneReturnProducts

--- backend/src/controllers/returnProducts.controller.js
+++ backend/src/controllers/returnProducts.controller.js
@@ -1,45 +1,66 @@
 const prisma = require("../prisma/prismaClient");
 
-const buildSearchFilter = (q)=>{
-    const stringFileds=[
-        
-    ]
-}
-
 const getReturnProducts = async (req, res) => {
   try {
     const returnProducts = await prisma.devolucion_producto.findMany({
-      include: { detalle_devolucion_producto: true }
+      include: { detalle_devolucion_producto: true },
     });
-    return res.status(200).json({returnProducts});
+    return res.status(200).json({ returnProducts });
   } catch (error) {
     return res.status(500).json({ error: "Error al obtener los productos" });
   }
 };
 
-const getOneReturnProdcts = async (req, res) =>{
-    const {id} = req.query;
-    try{
-        const returnProducts = await prisma.devolucion_producto.findMany({
-            where: {
-                OR:[
-                    {
-                        id_devolucion_productos: Number(id)
-                    },
-                    {
-
-                    }
-                ]
-
-            }
-        })
-        return res.status(200).json({returnProducts});
-    }catch(error){
-        return res.status(500).json({ error: "Error al obtener el producto" });
-    }
-}
+const getOneReturnProdcts = async (req, res) => {
+  const { q } = req.query;
+  const isNumber = !isNaN(q);
+  console.log(q);
+  const filter = isNumber
+  ?{
+    OR:[
+    {id_devolucion_product: {equals: Number(q)}},
+    {fecha_devolucion: {contains: q}},
+    {cantidad_total:{equals: Number(q)}},
+    {detalle_devolucion_producto:{
+      some:{
+        OR:[{
+          cantidad_devuelta:{equals: Number(q)}
+        }]
+      }
+    }}
+    ]
+  }
+  :{
+    OR:[{
+      nombre_responsable:{contains: q, mode: "insensitive"}
+    },
+    {OR:[{
+      detalle_devolucion_producto:{
+        some:{
+          OR:[{
+            nombre_producto:{contains: q, mode: "insensitive"},
+          },
+          {
+            motivo:{contains: q, mode: "insensitive"}
+          }
+        ]
+        }
+      }
+    }]}
+  ]
+  }
+  try {
+    const returnProducts = await prisma.devolucion_producto.findMany({
+      where: filter,
+      include: { detalle_devolucion_producto: true },
+    });
+    return res.status(200).json({ returnProducts });
+  } catch (error) {
+    return res.status(500).json({ error: "Error al obtener el producto" });
+  }
+};
 
 module.exports = {
   getReturnProducts,
-  getOneReturnProdcts
+  getOneReturnProdcts,
 };
--- backend/src/routes/returnProducts.routes.js
+++ backend/src/routes/returnProducts.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const returnProductsController = require("../controllers/returnProducts.controller");
 
 router.get("/", returnProductsController.getReturnProducts);
+router.get("/search", returnProductsController.getOneReturnProdcts);
 
 module.exports = router;
