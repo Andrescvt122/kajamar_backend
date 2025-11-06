5e51f096ecc1e01e42131ed82d2666cd77a65386
Andre <andrescvalencia.t@gmail.com>
2025-11-05 20:03:06 -0500

    creacion de controlador de productos random

---
+++ backend/src/controllers/products.controller.js
@@ -0,0 +1,42 @@
+const prisma = require("../prisma/prismaClient");
+
+const getRandomProduct = async (req,res)=>{
+    const {id_categoria} = req.params;
+    try {
+        const ids = await prisma.productos.findMany({
+            where: {
+                id_categoria: id_categoria
+            },
+            select: {
+                id_producto: true
+            }
+        })
+        if (!ids){
+            return res.status(404).json({
+                message: "No se encontraron productos"
+            })
+        }
+        while (true){
+        const id= Math.floor(Math.random() * ids.length)
+        const product = await prisma.productos.findUnique({
+            where: {
+                id_producto: ids[id].id_producto
+            }
+        })
+        if (product){
+            return res.status(200).json({
+                product
+            });
+        }
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Error al obtener el producto"
+        })
+    }
+}
+
+module.exports = {
+    getRandomProduct
+}
