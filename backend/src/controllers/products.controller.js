a3ae68e001b3e189ee0aa8a7e8387935b3fcc8f7
Andre <andrescvalencia.t@gmail.com>
2025-11-05 20:59:50 -0500

    arreglo en el controlador random productos

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -1,26 +1,27 @@
 const prisma = require("../prisma/prismaClient");
 
 const getRandomProduct = async (req,res)=>{
-    const {id_categoria} = req.params;
+    const {q} = req.query;
+    const id_categoria_int = parseInt(q)
+    console.log(q);
     try {
         const ids = await prisma.productos.findMany({
             where: {
-                id_categoria: id_categoria
-            },
-            select: {
-                id_producto: true
+                id_categoria: {
+                    equals: id_categoria_int
+                }
             }
         })
-        if (!ids){
+        if (ids.length === 0){
             return res.status(404).json({
                 message: "No se encontraron productos"
             })
         }
         while (true){
-        const id= Math.floor(Math.random() * ids.length)
+        const id_producto = Math.floor(Math.random() * ids.length)
         const product = await prisma.productos.findUnique({
             where: {
-                id_producto: ids[id].id_producto
+                id_producto: ids[id_producto].id_producto
             }
         })
         if (product){
--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/products.controller");
 
-router.get("/random/:id", productsController.getRandomProduct);
+router.get("/random", productsController.getRandomProduct);
 module.exports = router;
